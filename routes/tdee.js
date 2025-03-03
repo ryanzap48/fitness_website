acee11a3f6155d9fc80ec08c9653a63f1b2e7568
ryanzap48 <rjzappone@wpi.edu>
2025-03-03 16:12:35 -0500

    added some more tdee functionality

--- routes/tdee.js
+++ routes/tdee.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const activityMultipliers = {
+    "sedentary": 1.2,
+    "light": 1.375,  
+    "moderate": 1.55,      
+    "active": 1.725,      
+    "very_active": 1.9 
+}
+
 router.get('/', function(req, res, next) {
   res.render('tdee', { title: 'Hello' });
 });
 
 router.post('/', function(req, res, next) {
-    const { gender, age, weight, height, activity } = req.body;
-    res.send({ gender, age, weight, height, activity });
+    var { unit, gender, age, weight, height, activity } = req.body;
+    
+    var bmr, tdee ;
+
+    if (unit == 'imperial') {
+      weight *= 0.453592
+      height *= 2.54
+    }
+
+    if (gender == 'female') bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
+    else bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
+
+    tdee = bmr * (activityMultipliers[activity]);
+
+    res.render('tdee-result', { tdee: tdee.toFixed(0)});
 });
 
 module.exports = router;
